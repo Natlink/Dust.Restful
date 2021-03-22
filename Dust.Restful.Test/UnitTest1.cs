9ec63c8a3d09ad8bb1a1b0b3c454f5fe72319701
Enzo Lambert <natlink722@gmail.com>
2021-03-22 11:58:38 +0100

    Initial project setup

---
+++ Dust.Restful.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+using Dust.Restful.Core;
+using System;
+using Xunit;
+
+namespace Dust.Restful.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+            TestClass test = new TestClass();
+            Assert.NotNull(test);
+
+        }
+    }
+}
--- Dust.Restful/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Dust.Restful
-{
-    public class Class1
-    {
-    }
-}
